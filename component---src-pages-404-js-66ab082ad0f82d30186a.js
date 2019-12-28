412e8c15861fbe27d4fa1aca44a8bba823ab80b7
mastah <mas@tah.com>
2019-12-28 02:04:46 +0100

    Updates

--- component---src-pages-404-js-66ab082ad0f82d30186a.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{w2l6:function(e,t,n){"use strict";n.r(t);var l=n("FdF9"),a=n("Bl7J"),u=n("vrFN");t.default=function(){return l.default.createElement(a.a,null,l.default.createElement(u.a,{title:"404: Not found"}),l.default.createElement("h1",null,"NOT FOUND"),l.default.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-66ab082ad0f82d30186a.js.map
