e8d4f4c5c980148c94417f748b62ffa8446d8f8f
mastah <mas@tah.com>
2019-12-28 01:48:30 +0100

    Updates

---
+++ component---src-pages-page-2-js-882c9ee9e37c486c31a7.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{p5nM:function(e,t,a){"use strict";a.r(t);var l=a("FdF9"),n=a("Wbzz"),o=a("Bl7J"),u=a("vrFN");t.default=function(){return l.default.createElement(o.a,null,l.default.createElement(u.a,{title:"Page two"}),l.default.createElement("h1",null,"Hi from the second page"),l.default.createElement("p",null,"Welcome to page 2"),l.default.createElement(n.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-882c9ee9e37c486c31a7.js.map
